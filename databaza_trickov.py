e7eb5899345a98e4f04efd09644e6fd53adf5cc8
Glendi-code <misko.vojacek@gmail.com>
2025-11-10 22:59:58 +0100

    pridal som funkciu na doplnovanie riadkov tabulky cez konzolu, moznost prezriet si tabulku podla filtrov a ukoncenie aplikacie

--- databaza_trickov.py
+++ databaza_trickov.py
@@ -3,8 +3,27 @@ import sqlite3
 conn = sqlite3.connect("databaza_trickov.db")
 cur = conn.cursor()
 
-def add_trick(name, type, diff):
-    cur.execute('''INSERT INTO tricks (trick_name, trick_type, trick_difficulty)''')
+def add_trick(name, typ, diff):
+    cur.execute('''INSERT INTO tricks (trick_name, trick_type, trick_difficulty) VALUES (?, ?, ?)''', (name, typ, diff))
+    conn.commit()
+
+def view_list_filtered(filt):
+    if filt == "T":
+        filt = input("Type in the value you re seeking: ").strip()
+        cur.execute('''SELECT * FROM tricks WHERE trick_type=?''', filt)
+    else:
+        filt = input("Type in the value you re seeking: ").strip()
+        cur.execute('''SELECT * FROM tricks WHERE trick_difficulty=?''', filt)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def view_list_unfiltered():
+    cur.execute('''SELECT * FROM tricks''')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
 while True:
     print(
@@ -13,14 +32,23 @@ while True:
         "Delete trick: D\n" \
         "Update trick: U\n" \
         "Close: C")
-    choice = input("Type in your choice: ")
+    choice = input("Type in your choice: ").upper().strip()
 
-    if choice == "A" or choice == "a":
-        trick_name = input("Trick name: ")
-        trick_type = input("Trick type: ")
-        trick_difficulty = input("Trick difficulty: ")
+    if choice == "A":
+        trick_name = input("Trick name: ").strip()
+        trick_type = input("Trick type: ").strip()
+        trick_difficulty = input("Trick difficulty: ").strip()
+        add_trick(trick_name, trick_type, trick_difficulty)
 
-        
+    elif choice == "V":
+        filters = input("Would you like to filter by type(T), difficulty(D) or nothing(N): ").upper().strip()
+        if filters == "T" or filters == "D":
+            view_list_filtered(filters)
+        elif filters == "N":
+            view_list_unfiltered()
+
+    elif choice == "C":
+        break
 
 cur.close()
 conn.close()
